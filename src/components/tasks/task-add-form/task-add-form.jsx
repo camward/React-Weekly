1bf58dc45e3e67025e17359542ad885f17fb598a
Стяжкин Алексей Изосимович <a.styazhkin@magelan.ru>
2019-03-11 14:35:53 +0400

    форма добавления задачи. валидация

--- src/components/tasks/task-add-form/task-add-form.jsx
+++ src/components/tasks/task-add-form/task-add-form.jsx
@@ -1,10 +1,120 @@
 import React, {Component} from 'react'
 import './task-add-form.scss'
+import Button from "../../common/button/button"
+import Input from "../../common/input/input"
 
 export class TaskAddForm extends Component {
+
+    state = {
+        isFormValid: false,
+        formControls: {
+            description: {
+                value: '',
+                label: 'Описание задачи',
+                errorMessage: 'Добавьте описание',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 1
+                }
+            },
+            time: {
+                value: '',
+                label: 'Срок выполнения',
+                errorMessage: 'Укажите срок',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 1
+                }
+            }
+        }
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
+    addTask = () => {
+        console.log('add task')
+    }
+
+    validateControl(value, validation) {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+
+        return isValid
+    }
+
+    changeInput = (event, controlName) => {
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
+
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] = control
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
+        this.setState({
+            formControls,
+            isFormValid
+        })
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    shouldValidate={!!control.validation}
+                    errorMessage={control.errorMessage}
+                    onChange={event => this.changeInput(event, controlName)}
+                />
+            )
+        })
+    }
+
     render() {
         return (
-            <div>TaskAddForm</div>
+            <div className="task-add">
+                <form onSubmit={this.submitHandler} className="task-add_form">
+
+                    { this.renderInputs() }
+
+                    <Button
+                        className="primary"
+                        disabled={!this.state.isFormValid}
+                        onClick={this.addTask}
+                    >
+                        Добавить задачу
+                    </Button>
+                </form>
+            </div>
         )
     }
 }
--- src/components/tasks/tasks-list/tasks-list.jsx
+++ src/components/tasks/tasks-list/tasks-list.jsx
@@ -2,15 +2,21 @@ import React, {Component} from 'react'
 import './task-list.scss'
 import {Task} from './task/task'
 
+import {TaskAddForm} from "../task-add-form/task-add-form"
+
 export class TaskList extends Component {
     render() {
         return (
-            <ul className="taskList">
-                <Task className="taskList_item" />
-                <Task className="taskList_item" />
-                <Task className="taskList_item" />
-                <Task className="taskList_item" />
-            </ul>
+            <div>
+                <ul className="taskList">
+                    <Task className="taskList_item" />
+                    <Task className="taskList_item" />
+                    <Task className="taskList_item" />
+                    <Task className="taskList_item" />
+                </ul>
+
+                <TaskAddForm />
+            </div>
         )
     }
 }
